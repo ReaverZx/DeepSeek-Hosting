da2abffcd902d4443cef719170564851438ed87c
ReaverZx <ibrahim.seye0909@gmail.com>
2025-08-30 20:52:32 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from openai import OpenAI
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key=os.environ.get("HF_TOKEN"),
+)
+
+class Message(BaseModel):
+    content: str
+
+@app.post("/ask")
+def ask(message: Message):
+    completion = client.chat.completions.create(
+        model="deepseek-ai/DeepSeek-V3:together",
+        messages=[
+            {"role": "user", "content": message.content}
+        ],
+    )
+    return {"reply": completion.choices[0].message}
